a8a98b9725cf885c829fcf1a382234e8f130f6a1
dboerschlein <root@test.door43.org>
2015-07-14 10:03:42 +0300

    Snapshat of export.py as of 2015-01-29

--- obs/export.py
+++ obs/export.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 #
 #  Copyright (c) 2014 unfoldingWord
 #  http://creativecommons.org/licenses/MIT/
@@ -14,6 +14,7 @@ Exports OBS for given language to specified format.
 '''
 
 import os
+import re
 import sys
 import json
 import codecs
@@ -21,10 +22,35 @@ import shutil
 import urllib2
 import argparse
 
+lang_top_json = ''
+lang_bot_json = ''
+lang_json = ''
+
 api_url_txt = u'https://api.unfoldingword.org/obs/txt/1'
 api_url_jpg = u'https://api.unfoldingword.org/obs/jpg/1'
+api_test_door43 = u'http://test.door43.org'  # this one is http and not https
 api_abs = u'/var/www/vhosts/api.unfoldingword.org/httpdocs/obs/txt/1'
 
+MATCH_ALL = 0
+MATCH_ONE = 0
+
+def checkForStandardKeysJSON():
+    global lang_json
+    #
+    if 'topspace' not in lang_json.keys(): lang_json['topspace'] = '10pt' # nice for en,fr,es
+    if 'botspace' not in lang_json.keys(): lang_json['botspace'] = '12pt' # nice for en,fr,es
+    #if 'fontface' not in lang_json.keys(): lang_json['fontface'] = 'pagella' # backwards compatible to test changes for first pass only
+    if 'fontface' not in lang_json.keys(): lang_json['fontface'] = 'lato' # this is for production
+    #
+    if 'bodysize' not in lang_json.keys(): lang_json['bodysize'] = '8.5pt'
+    if 'tocsize' not in lang_json.keys(): lang_json['tocsize'] = '12pt'
+    if 'licsize' not in lang_json.keys(): lang_json['licsize'] = '8pt'
+    #
+    if 'bodybaseline' not in lang_json.keys(): lang_json['bodybaseline'] = '11pt'
+    if 'tocbaseline' not in lang_json.keys(): lang_json['tocbaseline'] = '16pt'
+    if 'licbaseline' not in lang_json.keys(): lang_json['licbaseline'] = '12pt'
+    #
+    if 'tocperpage' not in lang_json.keys(): lang_json['tocperpage'] = '22'
 
 def writeFile(outfile, p):
     makeDir(outfile.rpartition('/')[0])
@@ -86,8 +112,56 @@ def getRef(text, format='plain'):
     elif format == 'md':
         return u'*{0}*'.format(text)
     elif format == 'tex':
-        return u'\\startplacefigure[location=nonumber]\n  {{\\midaligned {{ \\tfx {{{{\\em {0}}}}}}}}}\n\\stopplacefigure'.format(text)
+        return u'\\startplacefigure[location=nonumber]\n  {{\\startalignment[middle] {{ \\tfx {{{{\\em {0}}}}}}} \\stopalignment }}\n\\stopplacefigure'.format(text)
     return text
+
+def export_matter(lang_message, format, img_res, lang):
+    global inItems
+    global MATCH_ALL
+    global MATCH_ONE
+    matchPipePattern = re.compile(ur"\s*([|])\s*",re.UNICODE)
+    matchBoldPattern = re.compile(ur"[*][*]\s*(.*?)\s*[*][*]",re.UNICODE)
+    matchUnderlinePattern = re.compile(ur"(\A|[^=])====\s*(.*?)\s*====([^=]|\Z)",re.UNICODE)
+    matchRedLetterPattern = re.compile(ur"(\A|[^:/])//(.*?)//([^/]|\Z)",re.UNICODE)
+    matchBlankLinePattern = re.compile(ur"^\s*$",re.UNICODE)
+    matchPatternLongURL = re.compile(ur"(http://[/\w\d,\._=+-]{41,9999})",re.UNICODE)
+    matchPatternURL = re.compile(ur"(http://[/\w\d,\._=+-]+)",re.UNICODE)
+    matchBulletPattern = re.compile(ur"^\s*[\*]\s+(.*)$",re.UNICODE)
+    matchChapterVersePattern = re.compile(ur"\s+(\d+:\d+)$",re.UNICODE)
+    '''
+    Exports JSON front/back matter to specificed format.
+    '''
+    j = u'\n\n'
+    if format == 'tex':
+        j = u'\n'
+    tmpsplit = lang_message.split(u'\n')
+    matter = [ ]
+    inItems = 0
+    def AnotherItem(matchobj):
+        global inItems
+        inItems += 1
+        ans = u'    \\item{' + matchobj.group(1) + u'}'
+        if (inItems == 1): ans = u'    \\startitemize\n' + ans
+        return ans
+    for single_line in tmpsplit:
+        copy = single_line
+        single_line = matchBlankLinePattern.sub(ur'    \\blank',single_line,MATCH_ALL)
+        (single_line, occurrences) = matchBulletPattern.subn(AnotherItem,single_line,MATCH_ONE)
+        if ((inItems > 0) and (occurrences == 0)):
+            inItems = 0
+            single_line = u'    \\stopitemize\n' + single_line
+        if (copy == single_line): single_line = u'    \\noindentation ' + single_line
+        single_line = matchRedLetterPattern.sub(ur'\1\\color[middlered]{\2}\3',single_line,MATCH_ALL)
+        single_line = matchUnderlinePattern.sub(ur'\1\\underbar{\2}\3',single_line,MATCH_ALL)
+        single_line = matchBoldPattern.sub(ur'{\\bf \1}',single_line,MATCH_ALL)
+        single_line = matchPipePattern.sub(ur' \\textbar \\space ',single_line,MATCH_ALL)
+        single_line = matchPatternLongURL.sub(ur'\\goto{\\midaligned \1}[url(\1)]',single_line,MATCH_ALL)
+        single_line = matchPatternURL.sub(ur'\\goto{\1}[url(\1)]',single_line,MATCH_ALL)
+        single_line = matchChapterVersePattern.sub(ur'~\1',single_line,MATCH_ALL)
+        matter.append(single_line)
+        #print u''.join([ u'copy=',copy ]).encode('utf-8')
+        #print u''.join([ u'single_line=',single_line ]).encode('utf-8')
+    return j.join(matter)
 
 def export(lang_json, format, img_res, lang):
     '''
@@ -109,35 +183,79 @@ def export(lang_json, format, img_res, lang):
         output.append(getRef(chp['ref'], format))
     return j.join(output)
 
+def getJSON(lang,entry,tmpent):
+    anyJSONe = entry.format(lang)
+    anyJSONf = '/'.join([api_url_txt, lang, anyJSONe])
+    anytmpf = '/'.join(['/tmp', tmpent]).format(lang)
+    getURL(anyJSONf, anytmpf)
+    if not os.path.exists(anytmpf):
+        print "Failed to get JSON {0} file into {1}.".format(anyJSONe,anytmpf)
+        sys.exit(1)
+    return anytmpf
+    
+def AnotherReplacement(matchobj):
+    global lang_json
+    keyword = matchobj.group(1)
+    if keyword in lang_json.keys(): return lang_json[keyword]
+    return 'nothing'
+
 def main(lang, outpath, format, img_res):
+    global MATCH_ALL
+    global MATCH_ONE
+    global lang_top_json
+    global lang_bot_json
+    global lang_json
+    #
+    matchChaptersPattern = re.compile(ur"===CHAPTERS===",re.UNICODE)
+    matchFrontMatterPattern = re.compile(ur"===FRONT\.MATTER===",re.UNICODE)
+    matchBackMatterPattern = re.compile(ur"===BACK\.MATTER===",re.UNICODE)
+    matchMiscPattern = re.compile(ur"<<<[\[]([^<>=]+)[\]]>>>",re.UNICODE)
+    #
+    toptmpf = getJSON(lang, 'obs-{0}-front-matter.json', '{0}-front-matter.tmp')
+    bottmpf = getJSON(lang, 'obs-{0}-back-matter.json', '{0}-back-matter.tmp')
+    lang_top_json = loadJSON(toptmpf, 'd')
+    lang_bot_json = loadJSON(bottmpf, 'd')
+    #
+    output_front = export_matter(lang_top_json['front-matter'], format, img_res, lang_top_json['language'])
+    output_back = export_matter(lang_bot_json['back-matter'], format, img_res, lang_bot_json['language'])
+    #
     jsonf = 'obs-{0}.json'.format(lang)
     lang_abs = os.path.join(api_abs, lang, jsonf)
     if os.path.exists(lang_abs):
         lang_json = loadJSON(lang_abs, 'd')
     else:
-        lang_url = '/'.join([api_url_txt, lang, jsonf])
-        tmpf = '/tmp/{0}'.format(jsonf)
-        getURL(lang_url, tmpf)
-        if not os.path.exists(tmpf):
-            print "Failed to get JSON file."
-            sys.exit(1)
+        #lang_url = '/'.join([api_url_txt, lang, jsonf])
+        #tmpf = '/tmp/{0}'.format(jsonf)
+        #getURL(lang_url, tmpf)
+        #if not os.path.exists(tmpf):
+        #   print "Failed to get JSON file."
+        #   sys.exit(1)
+        tmpf = getJSON(lang, jsonf, '{0}-body-matter.tmp')
         lang_json = loadJSON(tmpf, 'd')
-    output = export(lang_json['chapters'], format, img_res,
-                                                       lang_json['language'])
+    checkForStandardKeysJSON()
+    output = export(lang_json['chapters'], format, img_res, lang_json['language'])
+    #
     if format == 'tex':
         outlist = []
-        tex_url = '/'.join([api_url_txt, 'introTeXtOBS.tex'])
-        texf = '/tmp/introTeXtOBS.tex'
-        getURL(tex_url, texf)
-        if not os.path.exists(texf):
+        tex_url = '/'.join([api_test_door43, 'introTeXtOBS.tex'])
+        tmp_texF = '/tmp/introTeXtOBS.tex'
+        getURL(tex_url, tmp_texF)
+        if not os.path.exists(tmp_texF):
             print "Failed to get TeX template."
             sys.exit(1)
-        head = codecs.open(texf, 'r', encoding='utf-8').read()
-        outlist.append(head)
-        outlist.append(output)
-        outlist.append(u'\stoptext')
-        output = u'\n'.join(outlist)
-    writeFile(outpath, output)
+        template = codecs.open(tmp_texF, 'r', encoding='utf-8').readlines()
+        for single_line in template:
+            single_line = single_line.rstrip('\r\n')
+            if (matchChaptersPattern.search(single_line)): outlist.append(output)
+            elif (matchFrontMatterPattern.search(single_line)): outlist.append(output_front)
+            elif (matchBackMatterPattern.search(single_line)): outlist.append(output_back)
+            else:
+                ( single_line, occurrences ) = matchMiscPattern.subn(AnotherReplacement,single_line,MATCH_ALL)
+                while ( occurrences > 0):
+                    ( single_line, occurrences ) = matchMiscPattern.subn(AnotherReplacement,single_line,MATCH_ALL)
+                outlist.append(single_line)
+        full_output = u'\n'.join(outlist)
+        writeFile(outpath, full_output)
 
 
 if __name__ == '__main__':
